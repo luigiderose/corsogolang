1ff6e62ff0173a99bb3e2df3b355c94068133b1a
LuigiDeRose <de.roseluigi1986@gmail.com>
2018-12-13 00:40:47 +0100

    nuovo eserciziouno

---
+++ 008 eserciziouno/eserciziouno.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+//dichiara tre variabili x, y, z, assegnagli il valore 42, "Jamse Bond", true
+//1 stampali in un unico comando
+//2 sampali ognuno con un comando
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
