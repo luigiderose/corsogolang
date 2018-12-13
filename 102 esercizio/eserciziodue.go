35cb7d93693b332ef042fce17efd5f430b959f61
LuigiDeRose <de.roseluigi1986@gmail.com>
2018-12-13 20:52:57 +0100

    corretto esercizio 2 zerovalue

--- 102 esercizio/eserciziodue.go
+++ 102 esercizio/eserciziodue.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
+// esercizio 0 value
 var x int
 var y string
 var z bool
 
 func main() {
 
-	fmt.Printf("%T\n", x)
-	fmt.Printf("%T\n", y)
-	fmt.Printf("%T\n", z)
-
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
 }
