43b80dba953626da91d04a9c9e21d17fc0365f4c
Luigi De rose <l.derose@martec.it>
2018-12-13 18:11:18 +0100

    comandi git

---
+++ 000 comandi git/comandigit.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	Pull := "git pull https://github.com/luigiderose/corsogolang"
+	AggiungiFile := "git add --all"
+	Commit := "git commit -m"
+	Push := "git push"
+
+	fmt.Println("comandi git e descrizione")
+	fmt.Print(AggiungiFile)
+	fmt.Println(", da lanciare nella cartella di reposity principale per aggiungere i file")
+	fmt.Print(Commit)
+	fmt.Println(", da lanciare nella cartella di reposity principale per eseguire il commit")
+	fmt.Print(Push)
+	fmt.Println(", da lanciare nella cartella di reposity principale per eseguire il push sul server")
+	fmt.Print(Pull)
+	fmt.Println(", da lanciare per eseguire la pull di un")
+
+}
