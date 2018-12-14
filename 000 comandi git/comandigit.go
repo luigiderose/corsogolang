2481fca105fd574d9ac9e8e57296a8981da1f03e
Luigi De rose <l.derose@martec.it>
2018-12-14 15:33:55 +0100

    aggiunti comandi git

--- 000 comandi git/comandigit.go
+++ 000 comandi git/comandigit.go
@@ -6,12 +6,15 @@ import (
 
 func main() {
 
+	init := "git init"
 	Pull := "git pull https://github.com/luigiderose/corsogolang"
 	AggiungiFile := "git add --all"
 	Commit := "git commit -m"
 	Push := "git push"
 
 	fmt.Println("comandi git e descrizione")
+	fmt.Print(init)
+	fmt.Println(", da lanciare nella cartella per inizializzare la sincronizzazione git")
 	fmt.Print(AggiungiFile)
 	fmt.Println(", da lanciare nella cartella di reposity principale per aggiungere i file")
 	fmt.Print(Commit)
