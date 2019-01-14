5612156b5f7bd2164c72a6852b973c9c953dc989
Luigi De rose <l.derose@martec.it>
2019-01-14 17:51:24 +0100

    completato corso 106

--- 106 string value/stringvalue.go
+++ 106 string value/stringvalue.go
@@ -17,11 +17,25 @@ everybody`
 	fmt.Println(s1)
 	fmt.Printf("%T\n", s1)
 
-	nbytes := []byte(s)
-	nbytes1 := []byte(s1)
+	nbytes := []byte(s)   //array convertito in decimale della parola s
+	nbytes1 := []byte(s1) //array convertito in decimale della parola s1
 	// pur avendo uno spazio i caratteri non cambiano (qua sono stati convertiti in byte vedere tabella UTF 8)
 	fmt.Println(nbytes)
 	fmt.Println(nbytes1)
 	fmt.Printf("%T\n", nbytes)
 	fmt.Printf("%T\n", nbytes1)
+
+	//*****************    visualizzazione esadecimale della stringa 1^ visualizzazione rappresentazione UTF 8  **********************//
+	for i := 0; i < len(s); i++ {
+
+		fmt.Printf("%#U", s[i])
+	}
+
+	//*****************    visualizzazione esadecimale della stringa 2^ visualizzazione rappresentazione pura esadecimale **********************//
+	fmt.Println("")
+
+	for i, v := range s {
+
+		fmt.Printf("at index position %d we have hex %#x\n", i, v)
+	}
 }
