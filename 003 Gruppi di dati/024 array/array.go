01a7bde561304054eb72bbfe7af1c8437ce9ff88
Luigi De rose <l.derose@martec.it>
2019-02-15 13:51:46 +0100

    array e slice

--- 003 Gruppi di dati/024 array/array.go
+++ 003 Gruppi di dati/024 array/array.go
@@ -5,6 +5,11 @@ import (
 )
 
 func main() {
+	//to make an array
+	var x [5]int   //we need to specify the dimension in this case is  5 position 0,1,2,3,4.
+	fmt.Println(x) //print the contents of the array in 5 position 0.0.0.0.0
 
-	fmt.Println("pippo")
+	x[2] = 2       // i can assing a value in the position of the array
+	fmt.Println(x) // the result is [0.0.2.0.0]
+
 }
---
+++ 003 Gruppi di dati/025 slice/slice.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//COMPOSITE LITERALS
+	// x:= type{value} these is a slice. It allow you to group VALUES of the same TYPE
+	x := []int{5, 3, 4, 8, 2, 7, 9} //declaration
+	fmt.Println(x)                  //print
+
+}
