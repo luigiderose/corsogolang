01e3b29773d8dc7d4d3dc05d82b417ffa06deac7
Luigi De rose <l.derose@martec.it>
2019-02-25 11:18:53 +0100

    esercizio 8 grandi soddisfazioni tutto solo

--- 100_Esercizi/104 ninjaliv4/008 esercizio8/esercizio8.go
+++ 100_Esercizi/104 ninjaliv4/008 esercizio8/esercizio8.go
@@ -1,10 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
+	x := map[string][3]string{
+		"bond_james":      {"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": {"James Bond", "Literature", "Computer Science"},
+		"no_dr":           {"Being evil", "Ice cream", "Sunsets"},
+	}
 
-	fmt.Println("hello")
+	for i, v := range x {
+		fmt.Println("Last_firstname: ", i)
+		for _, z := range v {
+			fmt.Printf("\t \t \t \t %v\n", z)
+		}
+
+	}
+
 }
