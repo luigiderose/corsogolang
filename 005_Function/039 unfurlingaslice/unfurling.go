cf4ecf556b550d5f23a10cc1d86a84d6402e7f4e
LuigiDeRose <de.roseluigi1986@gmail.com>
2019-03-14 22:19:42 +0100

    unfurtling

--- 005_Function/039 unfurlingaslice/unfurling.go
+++ 005_Function/039 unfurlingaslice/unfurling.go
@@ -5,6 +5,19 @@ import (
 )
 
 func main() {
-	x := 1
-	fmt.Println(x)
+	xi := []int{2, 3, 4, 5, 6, 7, 8, 9}
+	y, k := sum("james", xi...)
+	fmt.Println(y, k)
+}
+
+func sum(z string, x ...int) (string, int) {
+
+	sum := 0
+
+	for _, v := range x {
+
+		sum += v
+
+	}
+	return z, sum
 }
